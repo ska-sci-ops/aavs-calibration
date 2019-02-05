b5c985edf3924952f38edcf15e45f5bc014132bd
nasiddi <nadina.siddiqui@ascent.software>
2019-02-05 10:02:14 +0100

    added bash install file

--- CalibrationDatabase/install.sh
+++ CalibrationDatabase/install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-echo Hello World
+echo Installing aavs-calibration database
+sudo apt update
+yes yes | sudo apt install mongodb
+yes yes | sudo apt-get install python-setuptools
+sudo python setup.py install
