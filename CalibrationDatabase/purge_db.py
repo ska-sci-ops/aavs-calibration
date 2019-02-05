e47d7497f0204ecfaf6b817189f690e5ae194fde
Nadina <nadinasiddiqui@msn.com>
2019-02-05 10:06:29 +0100

    updated purge_db

--- CalibrationDatabase/purge_db.py
+++ CalibrationDatabase/purge_db.py
@@ -1,8 +1,12 @@
-import sys
-import mongoengine
-def main(args):
-    mo.drop_database('aavs_test2')
+from db import Fit, Channel, Coefficient, connect
+db = connect('aavs', host='localhost', port=27017)
+
+
+def main():
+    Fit.drop_collection()
+    Channel.drop_collection()
+    Coefficient.drop_collection()
 
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main()
