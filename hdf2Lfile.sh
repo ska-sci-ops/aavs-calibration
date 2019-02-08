b103e0f5713910572f881c2f0709b9d02b1a1193
Randall Wayth <randall.wayth@gmail.com>
2019-02-08 13:26:51 +0800

    added some usage message to hdf2Lfile.sh

--- hdf2Lfile.sh
+++ hdf2Lfile.sh
@@ -1,9 +1,26 @@
 #!/bin/sh
 
-tsfile=`mktemp`
-hdffile="$1"
+# utility script to convert raw hdf5 files into L-files, including an option to do some time
+# averaging, which is passed to aavs_mwa_corr_converter.py (which does the real work)
+# also extract the starting time for the data into a file ending in ts_unix.txt
+# the start time is unix time (seconds since unix epoch)
+
 offset_hours=0
 naverage=3
+
+print_usage () {
+  echo "Usage:"
+  echo "$1 hdf_file [n_averages]"
+  echo "       default n_average: $naverage"
+  exit
+}
+
+if [ $# -eq 0 ] ; then
+  print_usage "$0"
+fi
+
+tsfile=`mktemp`
+hdffile="$1"
 
 if [ $# -gt 1 ] ; then
   naverage="$2"
