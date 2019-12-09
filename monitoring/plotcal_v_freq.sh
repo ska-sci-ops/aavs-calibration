507619ab86783ef80a429c432c4f6aa88f7c1e52
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2019-12-09 14:12:13 +0800

    corr

--- monitoring/plotcal_v_freq.sh
+++ monitoring/plotcal_v_freq.sh
@@ -24,21 +24,24 @@ curr_path=`pwd`
 cal_dtm=`basename $curr_path`
 echo "Real-time calibration path = $curr_path -> cal_dtm = $cal_dtm"
 
+echo "python ~/aavs-calibration/monitoring/plotcal_v_freq.py"
+python ~/aavs-calibration/monitoring/plotcal_v_freq.py
+
+# prepare directorios and copy images to WWW server :
 images_dir=${www_dir}/${station_name_lower}/${cal_dtm}
 
-echo "ssh aavs@aavs1-server \"mkdir -p ${images_dir}\""
-ssh aavs@aavs1-server "mkdir -p ${images_dir}"
+echo "ssh aavs@aavs1-server \"mkdir -p ${images_dir} ${www_dir}/${station_name_lower}/current\""
+ssh aavs@aavs1-server "mkdir -p ${images_dir} ${www_dir}/${station_name_lower}/current"
 
-echo "ssh aavs@aavs1-server \"ln -sf ${images_dir} ${www_dir}/${station_name_lower}/current\""
-ssh aavs@aavs1-server "ln -sf ${images_dir} ${www_dir}/${station_name_lower}/current"
+# echo "ssh aavs@aavs1-server \"ln -sf ${images_dir} ${www_dir}/${station_name_lower}/current\""
+# ssh aavs@aavs1-server "ln -sf ${images_dir} ${www_dir}/${station_name_lower}/current"
 
 echo "scp ~/aavs-calibration/monitoring/html/plot_calsol_index.html aavs@aavs1-server:${images_dir}/index.html"
 scp ~/aavs-calibration/monitoring/html/plot_calsol_index.html aavs@aavs1-server:${images_dir}/index.html
-
-echo "python ~/aavs-calibration/monitoring/plotcal_v_freq.py"
-python ~/aavs-calibration/monitoring/plotcal_v_freq.py
 
 
 echo "scp *.png aavs@aavs1-server:${images_dir}/"
 scp *.png aavs@aavs1-server:${images_dir}/
 
+echo "scp *.png aavs@aavs1-server:${www_dir}/${station_name_lower}/current/"
+scp *.png aavs@aavs1-server:${www_dir}/${station_name_lower}/current/
