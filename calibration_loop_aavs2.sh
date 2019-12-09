51f5df67b768506c089b0d7c1a785c7f62ea771c
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2019-12-09 11:38:45 +0800

    added options and removed --skip-db

--- calibration_loop_aavs2.sh
+++ calibration_loop_aavs2.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+options="" # was --skip-db
+if [[ -n "$1" && "$1" != "-" ]]; then
+   options="$1"
+fi
+
 # typical paremeters to execute a single calibration loop on the AAVS2 data (at the moment in debug mode : 
 
-echo "python ~/aavs-calibration/calibration_loop.py --config=/opt/aavs/config/aavs2.yml -i enp216s0f0 -d /data/ --skip-db"
-python ~/aavs-calibration/calibration_loop.py --config=/opt/aavs/config/aavs2.yml -i enp216s0f0 -d /data/ --skip-db
+echo "python ~/aavs-calibration/calibration_loop.py --config=/opt/aavs/config/aavs2.yml -i enp216s0f0 -d /data/ ${options}"
+python ~/aavs-calibration/calibration_loop.py --config=/opt/aavs/config/aavs2.yml -i enp216s0f0 -d /data/ ${options}
--- calibration_loop_eda2.sh
+++ calibration_loop_eda2.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+options="" # was --skip-db
+if [[ -n "$1" && "$1" != "-" ]]; then
+   options="$1"
+fi
+
 # typical paremeters to execute a single calibration loop on the AAVS2 data (at the moment in debug mode : 
 
-echo "python /home/aavs/aavs-calibration/calibration_loop.py --config=/opt/aavs/config/eda2_full_delayperant.yml -i enp216s0f0 -d /data/"
-python /home/aavs/aavs-calibration/calibration_loop.py --config=/opt/aavs/config/eda2_full_delayperant.yml -i enp216s0f0 -d /data/
+echo "python /home/aavs/aavs-calibration/calibration_loop.py --config=/opt/aavs/config/eda2_full_delayperant.yml -i enp216s0f0 -d /data/ ${options}"
+python /home/aavs/aavs-calibration/calibration_loop.py --config=/opt/aavs/config/eda2_full_delayperant.yml -i enp216s0f0 -d /data/ ${options}
