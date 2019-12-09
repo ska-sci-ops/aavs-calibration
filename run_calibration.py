854e8605b10093687c4dfd88e47937ca3a488fcc
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2019-12-09 13:20:06 +0800

    bugfix

--- run_calibration.py
+++ run_calibration.py
@@ -271,7 +271,7 @@ if __name__ == "__main__":
                       help="Station ID (as in the station configuratio file) [default: %]", type=int )                      
     parser.add_option("--station_name", dest="station_name", default="EDA2", # was AAVS1
                       help="Station ID (as in the station configuratio file) [default: %]" )                      
-     parser.add_option("-P", "--plots", '--do_plots', '--plot_solutions', action="store_true", dest="plot_solutions", default=True,
+    parser.add_option("-P", "--plots", '--do_plots', '--plot_solutions', action="store_true", dest="plot_solutions", default=True,
                       help="Plot calibration solutions and fits from the database [default: %defualt]")
     (conf, args) = parser.parse_args(argv[1:])
 
