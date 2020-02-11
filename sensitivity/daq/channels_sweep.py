7a04955a36429f8e5991c41a7edf57583027073d
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2020-02-11 17:14:14 +0800

    added startup time (30sec)

--- sensitivity/daq/channels_sweep.py
+++ sensitivity/daq/channels_sweep.py
@@ -1148,7 +1148,7 @@ if __name__ == "__main__":
        time.sleep( wait_time )
 
     # just to collect some initial data which is bad before collecting proper data later in the loop        
-    if False :
+    if True :
        # probably not needed anymore :
        station.send_channelised_data_continuous( 30 )
        time.sleep( 20 )
