c9ac32bd0486a5f66964a07e51ecb262d7c6e9ce
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2020-02-12 19:30:39 +0800

    corr

--- sensitivity/daq/daq_sensitivity.sh
+++ sensitivity/daq/daq_sensitivity.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
+# assuming full loop takes ~600 seconds 
+
+iteration=0
 while [ 1 ];
 do
-   
+   dtm=`date +%Y%m%d_%H%M%S`
+   mkdir -p ${dtm}
+
+   echo
+   echo "--------------------------------------------------- $iteration ---------------------------------------------------"   
+   echo "Date/time = $dtm"   
+   cd ${dtm}   
+   pwd
    echo "python /opt/aavs/bin/daq_receiver.py -i enp216s0f0 -d . -X --channel_samples=262144 -t 16 --continuous_period 4 --duration 620"
    python /opt/aavs/bin/daq_receiver.py -i enp216s0f0 -d . -X --channel_samples=262144 -t 16 --continuous_period 4 --duration 620 
+   cd ../
 
    while [[ ! -s started_channel_loop.txt ]];
    do
       echo "Waiting for new channel loop file ( started_channel_loop.txt )"
       sleep 1
    done
+
+   iteration=$(($iteration+1))
 done
