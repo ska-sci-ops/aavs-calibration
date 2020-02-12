67e7e875ce1a5e8e86b9cd055eefa50e48009129
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2020-02-12 20:09:39 +0800

    1800 seconds

--- sensitivity/daq/channels_sweep.sh
+++ sensitivity/daq/channels_sweep.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-interval=300
+# default EDA2:
+config_file=/opt/aavs/config/eda2_full_delayperant.yml
+if [[ -n "$1" && "$1" != "-" ]]; then
+   config_file=$1
+fi
+
+# interval between channel loops 
+interval=1800
 
 rm -f started_channel_loop.txt
 
@@ -13,8 +20,8 @@ do
    date   
    date +%s > started_channel_loop.txt
    # just one iteration over channels :      
-   echo "python ~/aavs-calibration/sensitivity/daq/channels_sweep.py --config=/opt/aavs/config/eda2_full_delayperant.yml --time_per_channel=28 --start_channel=51 --end_channel=450 --step-channel=13 --n_iterations=1"
-   python ~/aavs-calibration/sensitivity/daq/channels_sweep.py --config=/opt/aavs/config/eda2_full_delayperant.yml --time_per_channel=28 --start_channel=51 --end_channel=450 --step-channel=13 --n_iterations=1
+   echo "python ~/aavs-calibration/sensitivity/daq/channels_sweep.py --config=${config_file} --time_per_channel=28 --start_channel=51 --end_channel=450 --step-channel=13 --n_iterations=1"
+   python ~/aavs-calibration/sensitivity/daq/channels_sweep.py --config=${config_file} --time_per_channel=28 --start_channel=51 --end_channel=450 --step-channel=13 --n_iterations=1
    
    echo "rm -f started_channel_loop.txt"
    rm -f started_channel_loop.txt   
