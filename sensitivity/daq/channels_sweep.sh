5cc6d4872beaa66df9483a99dd1827fc846bd2db
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2020-02-14 17:58:51 +0800

    improvements

--- sensitivity/daq/channels_sweep.sh
+++ sensitivity/daq/channels_sweep.sh
@@ -6,8 +6,27 @@ if [[ -n "$1" && "$1" != "-" ]]; then
    config_file=$1
 fi
 
+start_channel=51
+if [[ -n "$2" && "$2" != "-" ]]; then
+   start_channel=$2
+fi
+
+end_channel=450
+if [[ -n "$3" && "$3" != "-" ]]; then
+   end_channel=$3
+fi
+
+step=13
+if [[ -n "$4" && "$4" != "-" ]]; then
+  step=$4
+fi
+
 # interval between channel loops 
 interval=1800
+if [[ -n "$5" && "$5" != "-" ]]; then
+   interval=$5
+fi
+
 
 rm -f started_channel_loop.txt
 
@@ -20,8 +39,8 @@ do
    date   
    date +%s > started_channel_loop.txt
    # just one iteration over channels :      
-   echo "python ~/aavs-calibration/sensitivity/daq/channels_sweep.py --config=${config_file} --time_per_channel=28 --start_channel=51 --end_channel=450 --step-channel=13 --n_iterations=1"
-   python ~/aavs-calibration/sensitivity/daq/channels_sweep.py --config=${config_file} --time_per_channel=28 --start_channel=51 --end_channel=450 --step-channel=13 --n_iterations=1
+   echo "python ~/aavs-calibration/sensitivity/daq/channels_sweep.py --config=${config_file} --time_per_channel=28 --start_channel=${start_channel} --end_channel=${end_channel} --step-channel=${step} --n_iterations=1"
+   python ~/aavs-calibration/sensitivity/daq/channels_sweep.py --config=${config_file} --time_per_channel=28 --start_channel=${start_channel} --end_channel=${end_channel} --step-channel=${step} --n_iterations=1
    
    echo "rm -f started_channel_loop.txt"
    rm -f started_channel_loop.txt   
