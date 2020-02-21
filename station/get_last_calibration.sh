03b4873b777c6e0f2647964832a848eb3d22d4e6
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2020-02-21 14:10:08 +0800

    script for getting latest station solutions

---
+++ station/get_last_calibration.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+station=eda2
+if [[ -n "$1" && "$1" != "-" ]]; then
+   station=$1
+fi
+station_upper=`echo $station | awk '{print toupper($1);}'`
+
+ch=-1 # means copy all txt files 
+if [[ -n "$2" && "$2" != "-" ]]; then
+   ch=$2
+fi
+
+local_caldir="/home/msok/Desktop/${station_upper}/real_time_calibration"
+
+last_cal_dir=`ssh ${station} "ls -dtr /data/real_time_calibration/2???_??_??-??:??/ |tail -1"`
+last_cal_dtm=`basename ${last_cal_dir}`
+
+echo "last_cal_dtm = $last_cal_dtm ( $last_cal_dir )"
+
+mkdir -p ${local_caldir}/${last_cal_dtm}
+cd ${local_caldir}/${last_cal_dtm}
+pwd
+
+if [[ $ch -ge 0 ]]; then
+   echo "rsync -avP ${station}:${last_cal_dir}/*${ch}.txt ."
+   rsync -avP ${station}:${last_cal_dir}/*${ch}.txt .
+else
+   echo "rsync -avP ${station}:${last_cal_dir}/*.txt ."
+   rsync -avP ${station}:${last_cal_dir}/*.txt .
+fi   
