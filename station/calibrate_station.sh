62f40787ff12439f4a9b399f96501e4dfd92a51e
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2020-04-17 16:22:26 +0800

    debug sleep added

--- station/calibrate_station.sh
+++ station/calibrate_station.sh
@@ -21,6 +21,7 @@ last_calib=`ls -tr *_ch${channel}*.pkl | tail -1`
 echo "Last calibration pickle file is $last_calib"
 
 echo "python ~/aavs-calibration/station/calibrate_station_newsoft.py -- --config=/opt/aavs/config/${station_name}.yml  --calibrate_station --calibrate_file=${last_calib}"
+sleep 5
 python ~/aavs-calibration/station/calibrate_station_newsoft.py -- --config=/opt/aavs/config/${station_name}.yml  --calibrate_station --calibrate_file=${last_calib}
 
 cd -
