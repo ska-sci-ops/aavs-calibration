f22c4daf868d4b900471e02a61f92ffc7737f2c8
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2020-04-17 16:29:29 +0800

    bugfix

--- station/calibrate_station.sh
+++ station/calibrate_station.sh
@@ -20,8 +20,8 @@ cd ${cal_dir}
 last_calib=`ls -tr *_ch${channel}*.pkl | tail -1`
 echo "Last calibration pickle file is $last_calib"
 
-echo "python ~/aavs-calibration/station/calibrate_station_newsoft.py -- --config=/opt/aavs/config/${station_name}.yml  --calibrate_station --calibrate_file=${last_calib}"
+echo "python ~/aavs-calibration/station/calibrate_station_newsoft.py --config=/opt/aavs/config/${station_name}.yml  --calibrate_station --calibrate_file=${last_calib}"
 sleep 5
-python ~/aavs-calibration/station/calibrate_station_newsoft.py -- --config=/opt/aavs/config/${station_name}.yml  --calibrate_station --calibrate_file=${last_calib}
+python ~/aavs-calibration/station/calibrate_station_newsoft.py --config=/opt/aavs/config/${station_name}.yml  --calibrate_station --calibrate_file=${last_calib}
 
 cd -
