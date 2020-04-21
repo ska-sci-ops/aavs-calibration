e0c9aa0b0340a37a477659672b48db795685f459
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2020-04-21 15:12:11 +0800

    rebust

--- station/create_pickle_files.sh
+++ station/create_pickle_files.sh
@@ -6,14 +6,15 @@ station_name=eda2
 if [[ -n "$1" && "$1" != "-" ]]; then
    station_name=$1
 fi
+station_name_lowercase=`echo $station_name | awk '{print tolower($1);}'`
 
 ch=0
 while [[ $ch -le 512 ]];
 do
    ch_str=`echo $ch | awk '{printf("%03d",$1);}'`
 
-   echo "~/aavs-calibration/station/make_station_pickle_file.sh $ch_str $station_name last"
-   ~/aavs-calibration/station/make_station_pickle_file.sh $ch_str $station_name last
+   echo "~/aavs-calibration/station/make_station_pickle_file.sh $ch_str $station_name_lowercase last"
+   ~/aavs-calibration/station/make_station_pickle_file.sh $ch_str $station_name_lowercase last
    
    ch=$(($ch+1))
 done
