7c10396d50d8f4cd8043b896a3fd2ef026657288
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2020-05-14 12:29:41 +0800

    corrections

--- station/observe_object.sh
+++ station/observe_object.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export PATH=~/aavs-calibration/station/pointing:$PATH
+
+
 do_init_station=0
 calibrate_station=1
 freq_channel=294
@@ -12,8 +15,8 @@ interval=3600
 data_dir=`date +%Y_%m_%d_$object`
 
 
-mkdir -p ${data_dir}
-cd ${data_dir}
+mkdir -p /data/${data_dir}
+cd /data/${data_dir}
 
 if [[ $do_init_station -gt 0 ]]; then
    echo "Initialising the station"
@@ -33,6 +36,16 @@ else
   echo "WARNING : station calibration is not required"
 fi   
 
+# kill old and start new pointing 
+echo "killall point_station_radec_loop.sh"
+killall point_station_radec_loop.sh 
+sleep 5 
 
-echo "nohup point_station_radec_loop.sh ${ra} ${dec} ${interval} 30 ${station} > pointing.out 2>&1 &"
-nohup point_station_radec_loop.sh ${ra} ${dec} ${interval} 30 ${station} > pointing.out 2>&1 &
+# start pointing :
+echo "nohup point_station_radec_loop.sh ${ra} ${dec} ${interval} 30 ${station} >> pointing.out 2>&1 &"
+nohup point_station_radec_loop.sh ${ra} ${dec} ${interval} 30 ${station} >> pointing.out 2>&1 &
+
+# start acuisition :
+echo "nohup /home/aavs/Software/aavs-system/src/build_new/acquire_station_beam -d ./ -t ${interval} -s 1048576 -c 4  -i enp216s0f0 -p 10.0.10.190 >> daq.out 2>&1 &"
+nohup /home/aavs/Software/aavs-system/src/build_new/acquire_station_beam -d ./ -t ${interval} -s 1048576 -c 4  -i enp216s0f0 -p 10.0.10.190 >> daq.out 2>&1 &
+
