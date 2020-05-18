727fd57217dbfaf6f4a7911b5116bdc633ff67f5
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2020-05-18 17:02:36 +0800

    less debug info (only on option debug)

--- station/calibration.py
+++ station/calibration.py
@@ -126,7 +126,7 @@ def read_phase_offsets( filename ) :
     return (antenna_arr,phase_offset_arr,count)
 
 
-def read_calibration_phase_offsets( phase_offset_file_X=None, phase_offset_file_Y=None, n_channels=8, n_pols=4 ) :
+def read_calibration_phase_offsets( phase_offset_file_X=None, phase_offset_file_Y=None, n_channels=8, n_pols=4, debug=False ) :
     
     calibration_coef = None 
 
@@ -146,12 +146,13 @@ def read_calibration_phase_offsets( phase_offset_file_X=None, phase_offset_file_
     
     if count_x == count_y :
        calibration_coef = numpy.zeros( (count_x, n_channels, n_pols ) , dtype=numpy.complex128)
-       print("calibration_coef.shape = %d x %d x %d" % (calibration_coef.shape[0],calibration_coef.shape[1],calibration_coef.shape[2]))
-
-    
-       print("Number of phase offsets agree between X and Y and is %d" % (count_x))
+       
+       if debug : 
+          print("calibration_coef.shape = %d x %d x %d" % (calibration_coef.shape[0],calibration_coef.shape[1],calibration_coef.shape[2]))   
+          print("Number of phase offsets agree between X and Y and is %d" % (count_x))
                                    
-       print("# ANT   |     X[deg]      |     Y[deg]       |    X_complex    |    Y_complex    |")
+          print("# ANT   |     X[deg]      |     Y[deg]       |    X_complex    |    Y_complex    |")
+          
        for i in range(0,count_x) :       
           phase_x_rad = phase_offset_arr_x[i]*(math.pi / 180.00)
           phase_y_rad = phase_offset_arr_y[i]*(math.pi / 180.00)
@@ -163,7 +164,8 @@ def read_calibration_phase_offsets( phase_offset_file_X=None, phase_offset_file_
           calibration_coef[i,:,0] = coeff_arr_x[i] 
           calibration_coef[i,:,3] = coeff_arr_y[i]
        
-          print(" %03d    |    %09.4f    |    %09.4f     |  %s  |  %s  |" % (i,phase_offset_arr_x[i],phase_offset_arr_y[i],coeff_arr_x[i],coeff_arr_y[i]))
+          if debug : 
+             print(" %03d    |    %09.4f    |    %09.4f     |  %s  |  %s  |" % (i,phase_offset_arr_x[i],phase_offset_arr_y[i],coeff_arr_x[i],coeff_arr_y[i]))
           
           
           
@@ -325,7 +327,7 @@ if __name__ == '__main__':
     if options.test_pickle_file is not None :
         test_calibration( options.test_pickle_file )
     else :
-        (calibration_coef) = read_calibration_phase_offsets( phase_offset_file_X , phase_offset_file_Y )
+        (calibration_coef) = read_calibration_phase_offsets( phase_offset_file_X , phase_offset_file_Y, debug=options.debug )
         ant_count = calibration_coef.shape[0]
         
         if options.debug :
