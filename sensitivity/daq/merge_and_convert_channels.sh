a70752313f30921368c7ae09460bd8ee0fd9066c
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2020-05-21 11:45:23 +0800

    station_name param added

--- sensitivity/daq/merge_and_convert_channels.sh
+++ sensitivity/daq/merge_and_convert_channels.sh
@@ -5,6 +5,11 @@ if [[ -n "$1" && "$1" != "-" ]]; then
    template="$1"
 fi
 
+station_name="eda2"
+if [[ -n "$2" && "$2" != "-" ]]; then
+   station_name=$2
+fi
+
 pwd
 
 for ch in `ls -d ${template}`
@@ -19,13 +24,13 @@ do
    # -H disables dumping of .bin files 
    # -i 0.2831 is integration time in seconds when --channel_samples=262144  samples (1.08 usec each) are collected 
    merge_path=`which hdf5_to_uvfits_all.sh`
-   echo "bash $merge_path -i 0.2831 -n 8140 -d merged/ -H -S 0 > merge.out"
-   bash $merge_path -i 0.2831 -n 8140 -d merged/ -H -S 0 > merge.out 
+   echo "bash $merge_path -i 0.2831 -n 8140 -d merged/ -H -S 0 -s ${station_name} > merge.out"
+   bash $merge_path -i 0.2831 -n 8140 -d merged/ -H -S 0 -s ${station_name} > merge.out 
    
    # move to separate sub-channel dirs :
    cd merged/
-   echo "/usr/local/bin/hdf2uvfits_zenith.sh \"-f ${ch}\""
-   /usr/local/bin/hdf2uvfits_zenith.sh "-f ${ch}"   
+   echo "/usr/local/bin/hdf2uvfits_zenith.sh \"-f ${ch} -s ${station_name}\""
+   /usr/local/bin/hdf2uvfits_zenith.sh "-f ${ch} -s ${station_name}"   
    cd ../
       
 
