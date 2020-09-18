05fc2592513d1ea6b81dfe339807ee56c2af542e
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2020-09-18 09:52:06 +0800

    auto-detection of station name from the config file added

--- station/observe_object.sh
+++ station/observe_object.sh
@@ -93,6 +93,13 @@ if [[ -n "${11}" && "${11}" != "-" ]]; then
 fi
 
 station=eda2
+if [[ -s /opt/aavs/config/station.yml ]]; then
+   station=`awk -v station_section=0 '{if(index($1,":")>0 && NF==1){if(index($1,"station")>0 ){station_section=1;}else{station_section=0;}}if(station_section>0){if($1=="name:"){station_name=$2;gsub("\"","",station_name);gsub("\r","",station_name);print tolower(station_name);}}}' /opt/aavs/config/station.yml`
+   echo "Station config file (or symbolik link) exists -> getting station_name = $station"
+else
+   echo "ERROR : /opt/aavs/config/station.yml file or symbolic link does not exist will use default station_name = $station or value passed in parameter -s"   
+   exit;
+fi
 if [[ -n "${12}" && "${12}" != "-" ]]; then
    station=${12}
 fi
