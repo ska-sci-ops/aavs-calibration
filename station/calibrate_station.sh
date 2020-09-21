011a448c76d49e1c277cc08ffc96a333112c712f
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2020-09-21 14:09:06 +0800

    added

--- station/calibrate_station.sh
+++ station/calibrate_station.sh
@@ -6,6 +6,7 @@ channel=204
 if [[ -n "$1" && "$1" != "-" ]]; then
    channel=$1
 fi
+channel_str=`echo $channel | awk '{printf("%03d",$1);}'`
 
 station_name=eda2
 if [[ -n "$2" && "$2" != "-" ]]; then
@@ -25,7 +26,7 @@ cal_dir=/data/real_time_calibration/last_calibration
 
 cd ${cal_dir}
 
-last_calib=`ls -tr *_ch${channel}*.pkl | tail -1`
+last_calib=`ls -tr *_ch${channel_str}*.pkl | tail -1`
 echo "Last calibration pickle file is $last_calib"
 
 echo "python ~/aavs-calibration/station/calibrate_station_newsoft.py --config=/opt/aavs/config/${station_name}.yml  --calibrate_station --calibrate_file=${last_calib} ${pol_swap_options}"
