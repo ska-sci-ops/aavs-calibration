8a223d807978cfde01b1f51a346d4c300ae69cd9
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2021-05-15 14:08:16 +0800

    eda2 script added

---
+++ monitoring/check_antenna_health_eda2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "check_antenna_health.sh - 100 1 - eda2 /storage/monitoring/integrated_data/eda2/"
+check_antenna_health.sh - 100 1 - eda2 /storage/monitoring/integrated_data/eda2/
