0228b2c559b1d7a27806c37a3905b02b72079c3c
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2021-05-15 14:14:22 +0800

    auto-copy

--- monitoring/check_antenna_health_eda2.sh
+++ monitoring/check_antenna_health_eda2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "check_antenna_health.sh - 100 1 - eda2 /storage/monitoring/integrated_data/eda2/"
-check_antenna_health.sh - 100 1 - eda2 /storage/monitoring/integrated_data/eda2/
+echo "check_antenna_health.sh - 100 1 - eda2 /storage/monitoring/integrated_data/eda2/ 1"
+check_antenna_health.sh - 100 1 - eda2 /storage/monitoring/integrated_data/eda2/ 1 
