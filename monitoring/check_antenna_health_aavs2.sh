a2169b74876e3633525b7f12fc02b5bce70ba561
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2021-05-17 17:03:29 +0800

    script for monitoring AAVS2 antenna health added

---
+++ monitoring/check_antenna_health_aavs2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "check_antenna_health.sh - 100 1 - aavs2 /storage/monitoring/integrated_data/aavs2/ 1"
+check_antenna_health.sh - 100 1 - aavs2 /storage/monitoring/integrated_data/aavs2/ 1 
