cbb5890f60fa44804bd2499861fb5a7e9a1b77c9
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2021-06-25 13:12:22 +0800

    added line with number of bad antennas

--- monitoring/check_antenna_health.py
+++ monitoring/check_antenna_health.py
@@ -629,8 +629,12 @@ def write_bad_antenna_html_header( out_bad_html_f , options, median_total_power_
    out_bad_html_f.write( "<h2>Antenna spectra compared with a median spectrum +/- %.1f x sigma_iqr :</h2>\n" % (options.threshold_in_sigma) )
    out_bad_html_f.write( "<ul>\n" )
    
-def write_bad_antenna_html_end( out_bad_html_f , options ) :
+def write_bad_antenna_html_end( out_bad_html_f , options, n_bad_and=0 ) :
    out_bad_html_f.write("</ul>\n")
+   
+   line = ("<br><h2>Number of antennas with one bad polarisation : %d </h2>\n<ul>\n" % (n_bad_ant))
+   out_bad_html_f.write( line )
+   
    out_bad_html_f.write("</body>\n")
    out_bad_html_f.write("</html>\n")
    
@@ -721,6 +725,7 @@ def check_antenna_health( hdf_file_template, options,
    comment = ("#   MED    MED      MED      %06d            %03d                %06d             %03d        REFERENCE\n" % (median_total_power_x,median_bad_channels_x,median_total_power_y,median_bad_channels_y))
    out_report_f.write( comment )
 
+   n_bad_ant_count = 0
    print("\tComparing antenna spectra with median spectrum ...")   
    for tile in range(0,nof_tiles) : 
       d_test=f_data[tile] 
@@ -776,6 +781,8 @@ def check_antenna_health( hdf_file_template, options,
             flag   = "OK"
          else :
             status = "BAD"
+            n_bad_ant_count += 1
+            
             flag = flag_x + "," + flag_y
             line = "%s : %05d  %05d , %05d %s\n" % (antname,ant_idx,tile,ant,flag)
             out_bad_f.write( line )
@@ -817,7 +824,7 @@ def check_antenna_health( hdf_file_template, options,
    out_bad_f.close()         
    
    # write end of the file and close :   
-   write_bad_antenna_html_end( out_bad_html_f , options )
+   write_bad_antenna_html_end( out_bad_html_f , options, n_bad_ant_count )
    out_bad_html_f.close()
       
 #  CLOSE HDF5 files :
