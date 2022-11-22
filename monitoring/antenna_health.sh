c3610e351f25bc8abba5aa387e411a686d7c8a93
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2022-11-22 17:26:00 +0800

    directory param added

--- monitoring/antenna_health.sh
+++ monitoring/antenna_health.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+datadir=/storage/monitoring/integrated_data/eda2/
+if [[ -n "$1" && "$1" != "-" ]]; then
+   datadir="$1"
+fi
+
 while [ 1 ];
 do
    echo
    date
    
-   echo "time check_antenna_health_eda2.sh"
-   time check_antenna_health_eda2.sh   
+   echo "time check_antenna_health_eda2.sh ${datadir}"
+   time check_antenna_health_eda2.sh ${datadir}
    
    echo "sleep 3600"
    sleep 3600
--- monitoring/check_antenna_health_eda2.sh
+++ monitoring/check_antenna_health_eda2.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-echo "check_antenna_health.sh - 100 1 - eda2 /storage/monitoring/integrated_data/eda2/ 1"
-check_antenna_health.sh - 100 1 - eda2 /storage/monitoring/integrated_data/eda2/ 1 
+datadir=/storage/monitoring/integrated_data/eda2/
+if [[ -n "$1" && "$1" != "-" ]]; then
+   datadir="$1"
+fi
+
+echo "check_antenna_health.sh - 100 1 - eda2 ${datadir} 1"
+check_antenna_health.sh - 100 1 - eda2 ${datadir} 1 
