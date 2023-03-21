88ceab29538d7a83dc790eee429defdbe6a69d93
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2023-03-21 20:12:47 +0800

    added fitting

--- calibration_loop_eda2.sh
+++ calibration_loop_eda2.sh
@@ -26,3 +26,7 @@ fi
 # typical paremeters to execute a single calibration loop on the AAVS2 data (at the moment in debug mode : 
 echo "python /home/aavs/aavs-calibration/calibration_loop.py --config=/opt/aavs/config/eda2.yml -i enp216s0f0 -d /data/ ${options}"
 python /home/aavs/aavs-calibration/calibration_loop.py --config=/opt/aavs/config/eda2.yml -i enp216s0f0 -d /data/ ${options} 
+
+export PATH=~/aavs-calibration/station/:$PATH
+echo "~/aavs-calibration/station/fit_all_ant_amplitudes.sh 1 1 1 - 2 _amp > fit.out 2>&1"
+~/aavs-calibration/station/fit_all_ant_amplitudes.sh 1 1 1 - 2 _amp > fit.out 2>&1
