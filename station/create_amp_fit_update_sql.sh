5c120dc11bdd0ea546c4a712124e6fba0cbccb0c
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2023-03-21 20:10:33 +0800

    station Id added in SQL updates

--- station/create_amp_fit_update_sql.sh
+++ station/create_amp_fit_update_sql.sh
@@ -5,6 +5,19 @@ if [[ -n "$1" && "$1" != "-" ]]; then
    fit_time="$1"
 fi
 
+station_id=2
+if [[ -n "$2" && "$2" != "-" ]]; then
+   station_id=$2
+fi
+
+echo "#######################################"
+echo "PARAMETERS :"
+echo "#######################################"
+echo "fit_time = $fit_time"
+echo "station_id = $station_id"
+echo "#######################################"
+
+
 sqlfile=amp_fitted.sql
 
 rm -f ${sqlfile}
@@ -15,5 +28,5 @@ do
    amp_x_list=`cat $fitted_file | awk -v amp_list="" '{if($1!="#"){if(length(amp_list)<=0){amp_list=$2;}else{amp_list=amp_list "," $2;}}}END{print amp_list;}'`
    amp_y_list=`cat $fitted_file | awk -v amp_list="" '{if($1!="#"){if(length(amp_list)<=0){amp_list=$4;}else{amp_list=amp_list "," $4;}}}END{print amp_list;}'`
    
-   echo "UPDATE calibration_solution set x_amp_fit='{"$amp_x_list"}',y_amp_fit='{"$amp_y_list"}' where fit_time='"$fit_time"' and ant_id=$ant;" >> ${sqlfile}
+   echo "UPDATE calibration_solution set x_amp_fit='{"$amp_x_list"}',y_amp_fit='{"$amp_y_list"}' where fit_time='"$fit_time"' and ant_id=$ant and station_id=$station_id;" >> ${sqlfile}
 done
