24c7bf638d8c8f0896b78aeee6f9bdc99c88076c
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2023-03-25 15:19:36 +0800

    test

--- runtime_eda2.sh
+++ runtime_eda2.sh
@@ -4,3 +4,9 @@ export PATH=~/aavs-calibration/:~/aavs-calibration/station:$PATH
 source /home/aavs/Software/miriad/miriad/MIRRC.sh
 export PATH=$PATH:$MIRBIN:/usr/local/bin/
 PGHOST=10.128.16.52
+
+
+echo $PATH
+which corr2uvfits
+
+corr2uvfits
