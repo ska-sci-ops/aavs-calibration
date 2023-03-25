3bea0150b39b5cd131c409dd8d0a5a0daae6d957
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2023-03-25 15:51:04 +0800

    correction

--- runtime_eda2.sh
+++ runtime_eda2.sh
@@ -3,7 +3,7 @@ source /opt/aavs/python/bin/activate
 export PATH=~/aavs-calibration/:~/aavs-calibration/station:$PATH
 source /home/aavs/Software/miriad/miriad/MIRRC.sh
 export PATH=$PATH:$MIRBIN:/usr/local/bin/
-PGHOST=10.128.16.52
+export PGHOST=10.128.16.52
 
 
 echo $PATH
