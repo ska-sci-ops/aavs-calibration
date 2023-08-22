259dbdfa22e218de7ca5619a6508b360b87313a1
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2023-08-22 15:01:51 +0800

    corrections

--- freq_channel_loop_aavs2.sh
+++ freq_channel_loop_aavs2.sh
@@ -34,7 +34,7 @@ ux=$start_ux
 while [[ $ux -lt $end_ux ]];
 do
    echo
-   echo "Loop $loop_count started at:"
+   echo "Loop $loop_count started at unixtime = $ux :"
    date
    
 #   echo "python ~/aavs-calibration/calibration_loop.py --config=/opt/aavs/config/aavs2.yml -i enp216s0f0 --do_not_calibrate --directory=$curr_dir"
@@ -44,6 +44,7 @@ do
    echo "python ~/aavs-calibration/calibration_loop_aavs2.py --config=/opt/aavs/config/aavs2.yml -i enp216s0f0 --do_not_calibrate --directory=${curr_dir} --daq_library=/opt/aavs/lib/libaavsdaq_fast_corr.so --samples=917504 --first_channel=64 --last_channel=448"
    python ~/aavs-calibration/calibration_loop_aavs2.py --config=/opt/aavs/config/aavs2.yml -i enp216s0f0 --do_not_calibrate --directory=${curr_dir} --daq_library=/opt/aavs/lib/libaavsdaq_fast_corr.so --samples=917504 --first_channel=64 --last_channel=448
    
+   ux=`date +%s`
    loop_count=$(($loop_count+1))
 done
 
