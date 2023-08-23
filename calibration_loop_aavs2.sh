1937be4da27e7afffdf15ed164a9604b4cc975e6
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2023-08-23 13:11:01 +0800

    correction to save to proper directory

--- calibration_loop_aavs2.sh
+++ calibration_loop_aavs2.sh
@@ -30,8 +30,8 @@ fi
 
 
 # typical paremeters to execute a single calibration loop on the AAVS2 data (at the moment in debug mode : 
-echo "python ~/aavs-calibration/calibration_loop.py --config=/opt/aavs/config/aavs2.yml -i enp216s0f0 -d /data/ ${options}"
-python ~/aavs-calibration/calibration_loop.py --config=/opt/aavs/config/aavs2.yml -i enp216s0f0 -d /data/ ${options}
+echo "python ~/aavs-calibration/calibration_loop.py --config=/opt/aavs/config/aavs2.yml -i enp216s0f0 -d /data/real_time_calibration/ ${options}"
+python ~/aavs-calibration/calibration_loop.py --config=/opt/aavs/config/aavs2.yml -i enp216s0f0 -d /data/real_time_calibration/ ${options}
 
 echo "post_calibration_actions.sh AAVS2"
 post_calibration_actions.sh AAVS2 
