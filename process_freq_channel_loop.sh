49038af9f7d4939d66a984a32a2a1938b1821f07
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2023-09-13 16:00:50 +0800

    added option : --do_not_update_last_cal

--- process_freq_channel_loop.sh
+++ process_freq_channel_loop.sh
@@ -13,8 +13,8 @@ station_name="AAVS2"
 for dir in `ls -d ${template}`
 do
    cd ${dir}
-   echo "python ~/aavs-calibration/run_calibration.py -D . --station_id ${station_id} --station_name ${station_name} --show-output --keep_uv_files --no_calibration"
-   python ~/aavs-calibration/run_calibration.py -D . --station_id ${station_id} --station_name ${station_name} --show-output --keep_uv_files --no_calibration
+   echo "python ~/aavs-calibration/run_calibration.py -D . --station_id ${station_id} --station_name ${station_name} --show-output --keep_uv_files --no_calibration --do_not_update_last_cal"
+   python ~/aavs-calibration/run_calibration.py -D . --station_id ${station_id} --station_name ${station_name} --show-output --keep_uv_files --no_calibration --do_not_update_last_cal
    cd ..
 done
 
