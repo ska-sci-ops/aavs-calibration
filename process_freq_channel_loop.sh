b5de5119d3fd3199de98baa0201b284f37af393a
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2023-09-14 19:51:38 +0800

    added conversion from .uvfits to .uv

--- process_freq_channel_loop.sh
+++ process_freq_channel_loop.sh
@@ -15,6 +15,28 @@ do
    cd ${dir}
    echo "python ~/aavs-calibration/run_calibration.py -D . --station_id ${station_id} --station_name ${station_name} --show-output --keep_uv_files --no_calibration --do_not_update_last_cal"
    python ~/aavs-calibration/run_calibration.py -D . --station_id ${station_id} --station_name ${station_name} --show-output --keep_uv_files --no_calibration --do_not_update_last_cal
+
+   # convert .uvfits to .uv files:   
+   for uvfitsfile in `ls *.uvfits`
+   do
+      src=${uvfitsfile%%.uvfits}
+      
+      echo "fits op=uvin in=\"$uvfitsfile\" out="${src}.uv" options=compress"
+      fits op=uvin in="$uvfitsfile" out="${src}.uv" options=compress
+      
+      echo "puthd in=${src}.uv/jyperk value=1310.0"
+      puthd in=${src}.uv/jyperk value=1310.0
+      
+      echo "puthd in=${src}.uv/systemp value=200.0"
+      puthd in=${src}.uv/systemp value=200.0
+      
+      echo "uvcat vis=${src}.uv stokes=xx out=${src}_XX.uv"
+      uvcat vis=${src}.uv stokes=xx out=${src}_XX.uv
+      
+      echo "uvcat vis=${src}.uv stokes=yy out=${src}_YY.uv"
+      uvcat vis=${src}.uv stokes=yy out=${src}_YY.uv
+   done
+   
    cd ..
 done
 
